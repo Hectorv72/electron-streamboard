769a86adfd692fc94592dd87633a42307928a52e
Hectorv72 <hectorvaldezfsa13@gmail.com>
2022-08-16 16:39:12 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const win = new BrowserWindow({ frame: false })
+
+app.whenReady().then(() => {
+  // Register a 'CommandOrControl+X' shortcut listener.
+  const ret = globalShortcut.register('CommandOrControl+X', () => {
+    console.log('CommandOrControl+X is pressed')
+  })
+
+  if (!ret) {
+    console.log('registration failed')
+  }
+
+  // Check whether a shortcut is registered.
+  console.log(globalShortcut.isRegistered('CommandOrControl+X'))
+})
+
+app.on('will-quit', () => {
+  // Unregister a shortcut.
+  globalShortcut.unregister('CommandOrControl+X')
+
+  // Unregister all shortcuts.
+  globalShortcut.unregisterAll()
+})
+
